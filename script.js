70f390b426df8cbbeac9b9c7f392057c78325b21
Victor Kovalchuk <vict02r05@gmail.com>
2025-05-08 18:17:21 +0300

    add overflow styles to the body if the modal window is open; replace for loop to the forEach method; add preventDefault to the modal window triggers

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+// Modal window START
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const body = document.body;
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
+
+  body.style.overflow = 'hidden';
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
+  body.style.overflow = '';
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btnOpenModal => btnOpenModal.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +32,5 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Modal window END
