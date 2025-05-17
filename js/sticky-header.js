cdecc7ce7e739a041e7715bef69653a5a434a2d0
Victor Kovalchuk <vict02r05@gmail.com>
2025-05-17 14:08:46 +0300

    add sticky header (using IntersectionObserver API)

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,14 +1,23 @@
 'use strict';
 
 // Sticky Header START
-const initialCoords = section1.getBoundingClientRect();
 
-window.addEventListener('scroll', function () {
-  if (window.scrollY > initialCoords.y) {
-    nav.classList.add('sticky');
-  } else {
-    nav.classList.remove('sticky');
-  }
-})
+const header = document.querySelector('.header');
+
+const observer = new IntersectionObserver(function (entries, observer) {
+  const [entry] = entries;
+
+    if (entry.isIntersecting) {
+      nav.classList.remove('sticky');
+    } else {
+      nav.classList.add('sticky');
+    }
+}, {
+  root: null,
+  rootMargin: `-${nav.offsetHeight}px 0px 0px 0px`,
+  threshold: 0
+});
+
+observer.observe(header);
 
 // Sticky Header END
--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   })
+// }
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2]
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+
+// observer.observe(document.querySelector('#section--1'));
